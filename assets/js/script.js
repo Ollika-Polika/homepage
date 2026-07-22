befe49721031a5b76d12c6b2ce4cefded95e9442
Olika <rakkastantalvi@gmail.com>
2026-07-22 15:34:58 +0300

    Add Highlight active navigation link (#15)

--- assets/js/script.js
+++ assets/js/script.js
@@ -29,3 +29,48 @@ backToTopButton.addEventListener("click", () => {
     });
 
 });
+
+/* ===================================================== */
+/*                  ACTIVE NAVIGATION                     */
+/* ===================================================== */
+
+const sections = document.querySelectorAll("section");
+
+const navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", () => {
+
+    let currentSection = "";
+
+    sections.forEach(section => {
+
+        const sectionTop = section.offsetTop - 120;
+
+        const sectionHeight = section.offsetHeight;
+
+        if (
+            window.scrollY >= sectionTop &&
+            window.scrollY < sectionTop + sectionHeight
+        ) {
+
+            currentSection = section.getAttribute("id");
+
+        }
+
+    });
+
+    navLinks.forEach(link => {
+
+        link.classList.remove("active");
+
+        if (
+            link.getAttribute("href") === `#${currentSection}`
+        ) {
+
+            link.classList.add("active");
+
+        }
+
+    });
+
+});
